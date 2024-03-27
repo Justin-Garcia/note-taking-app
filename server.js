97954de5eabd239c9564dfb991174adc17051357
Justin Garcia <justingarcia1901@gmail.com>
2024-03-27 11:56:38 -0400

    updated server, notes file, and imported the files into their respective documents

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// GET route for the homepage
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './develop/public/index.html'));
+});
+
+// GET route for the notes page
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './develop/public/notes.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
